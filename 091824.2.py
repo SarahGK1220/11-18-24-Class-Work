ad6fddd7536a245c7b027942873e2914a65ccd0f
SarahGK1220 <SarahGK1220@gmail.com>
2024-11-18 15:19:11 -0600

    First file !! Yay (:

---
+++ 091824.2.py
@@ -0,0 +1,24 @@
+
+import math as m
+
+print(m.sqrt(30000000))
+
+m.pi
+
+
+from math import pi, sqrt
+
+print(pi)
+
+from math import *
+
+
+
+
+import matplotlib.pyplot as plt
+
+plt.plot([1,2,3],[4,5,6])
+
+plt.show()
+
+
